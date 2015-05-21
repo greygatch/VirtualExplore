60002b8f9814f9d6da1b4e9f1b840bda14168f64
Evan Gatchell <greygatch@gmail.com>
2015-05-20 20:49:14 -0700

    clyde joins the team

--- client/views/general/home.js
+++ client/views/general/home.js
@@ -36,23 +36,24 @@ angular.module('poseidon')
       var lng = result[0].geometry.location.F;
       console.log(result[0]);
       location = result[0].formatted_address;
+      $scope.location = result[0].formatted_address;
       Map.create('#mapDiv', lat, lng, 10);
-      $window.jQuery.get("http://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" + location.toUpperCase() + "&api_key=ea15c092788c7cfb6911fa14efe1d88f&per_page=10&format=json&nojsoncallback=1",
+      $window.jQuery.get('http://api.flickr.com/services/rest/?method=flickr.photos.search&tags=' + location.toUpperCase() + '&api_key=ea15c092788c7cfb6911fa14efe1d88f&per_page=10&format=json&nojsoncallback=1',
       function(data){
         $scope.$apply(function(){
           $scope.photos = data.photos.photo;
-          console.log($scope.photos, 'EEEEEE');
         });
-        $window.jQuery.get("http://api.wunderground.com/api/bdce31546b59e7b1/geolookup/forecast/q/" + location.toUpperCase() + ".json", function(response){
+        $window.jQuery.get('http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20location=%22'+location.trim()+'%22&format=json&u=c', function(response){
+          console.log(response, '!!!!!!!!!!!!');
           $scope.$apply(function(){
             $scope.weather = response.forecast.simpleforecast.forecastday[0];
           });
-          $window.jQuery.getJSON("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=revisions&gsrsearch="+location+"&rvprop=content&rvsection=0&callback=?", function(info){
+          $window.jQuery.getJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=revisions&gsrsearch='+location+'&rvprop=content&rvsection=0&callback=?', function(info){
             var firstPageResult = Object.keys(info.query.pages)[0];
             $scope.$apply(function(){
               $scope.pageId = info.query.pages[firstPageResult].pageid;
             });
-            // $window.jQuery.getJSON("http://twitter.com/search?q=place%3A247f43d441defc03", function(twitterResponse){
+            // $window.jQuery.getJSON('http://twitter.com/search?q=place%3A247f43d441defc03', function(twitterResponse){
             //   console.log(twitterResponse, 'woot');
             // });
             // var contentKey = Object.keys(info.query.pages[firstPageResult].revisions[0])[2];
@@ -62,8 +63,8 @@ angular.module('poseidon')
             // pageContent = pageContent.replace(/<[^>]*>/g, '');
             // $('#article').append(pageContent);
           });
-        })
-      })
+        });
+      });
     });
-  }
+  };
 });
